a324d972c928cb4584e46fa5c5258a76f54179fc
Alan Eicker <alaneicker@gmail.com>
2021-01-20 10:44:17 -0600

    sets up database

---
+++ src/server/db.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  socketPath: process.env.SOCKET_PATH,
+  host: process.env.HOST,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+pool.config.connectionLimit = 400;
+
+const db = (options) => {
+  const { query, data, isArray } = options;
+
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      console.log(err);
+      if (err) {
+        console.log('DATABASE ERROR: ', err);
+        connection.release();
+        reject(err);
+      }
+      connection.query(query, data, (err, result) => {
+        connection.release();
+        if (err) {
+          reject(err);
+        } else {
+          resolve(isArray === false ? result[0] : result);
+        }
+      });
+    });
+  });
+};
+
+module.exports = db;
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const db = require('./db');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send([
-    { id: 1, title: 'Cut Grass', isComplete: false },
-    { id: 2, title: 'Wash Bathrooms', isComplete: false },
-    { id: 3, title: 'Paint Bedroom', isComplete: true },
-    { id: 4, title: 'Walk Dogs', isComplete: false },
-  ]);
+  db({ query: `SELECT * FROM todos` })
+    .then((data) => res.send(data))
+    .catch((err) => console.log(err));
 });
 
 router.get('/:id', (req, res) => {});
