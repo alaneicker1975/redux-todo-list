76c9de64ef3195c1dcc08730ca105d903dc478b4
Alan Eicker <alaneicker@gmail.com>
2021-01-21 15:49:10 -0600

    removes redux-thunk

--- src/client/store.js
+++ src/client/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
 import reducer from './reducers';
 import api from './middleware/api';
 
-const store = createStore(reducer, applyMiddleware(ReduxThunk, api));
+const store = createStore(reducer, applyMiddleware(api));
 
 export default store;
