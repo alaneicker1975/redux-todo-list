d7669951b65f77e454d42e7e5d4b1e7c34f66e4f
Alan Eicker <alaneicker@gmail.com>
2021-01-20 21:59:02 -0600

    Update db.js

--- src/server/db.js
+++ src/server/db.js
@@ -42,23 +42,4 @@ class DB {
     });
 }
 
-// const db = ({ query, data, isArray }) =>
-//   new Promise((resolve, reject) => {
-//     pool.getConnection((err, connection) => {
-//       if (err) {
-//         console.log('DATABASE ERROR: ', err);
-//         connection.release();
-//         reject(err);
-//       }
-//       connection.query(query, data, (err, result) => {
-//         connection.release();
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(isArray === false ? result[0] : result);
-//         }
-//       });
-//     });
-//   });
-
 module.exports = DB;
