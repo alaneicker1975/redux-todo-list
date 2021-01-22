0ea822b212d172737f91aa3b5f93489bbc280467
Alan Eicker <alaneicker@gmail.com>
2021-01-21 20:06:27 -0600

    Moves withEventHandlers to Todos component

--- src/client/components/Todos/Todos.jsx
+++ src/client/components/Todos/Todos.jsx
@@ -1,55 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGet, fetchPut, fetchPatch, fetchDelete } from '../../actions';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Button, FormField, Header } from '@atomikui/core';
 import AddTodoForm from '../AddTodoForm';
-import withEventHandlers from './withEventHandlers';
 
-const Todos = ({ headerText, todos, onDelete, onAdd, onUpdate }) => (
-  <div className="todo-list">
-    <Header
-      backgroundColor="#1f2a30"
-      logoText={<span style={{ marginLeft: '8px' }}>{headerText}</span>}
-      logoFontSize={36}
-      logoFontColor="white"
-      logoFont="'Barlow Condensed', Arial, Helvetica, sans-serif"
-      linkColor="white"
-      menuToggleColor="white"
-    />
-    <AddTodoForm onSubmit={(title) => onAdd({ title })} />
-    <ul>
-      {todos.map(({ id, title, isComplete }) => (
-        <li key={id}>
-          <Button
-            className="todo-list__status-btn"
-            size="sm"
-            checked={isComplete}
-            theme={isComplete ? 'lime' : 'red'}
-            onClick={() => onUpdate(id, { isComplete: !isComplete })}
-            aria-label={isComplete ? 'done' : 'pending'}
-            title="click to update status"
-          />
-          <FormField
-            aria-label="todo title"
-            className="todo-list__input"
-            defaultValue={title}
-            onBlur={(e) => onUpdate(id, { title: e.target.value })}
-          />
-          <Button
-            className="todo-list__delete-btn"
-            theme="hollow"
-            size="sm"
-            onClick={() => onDelete(id)}
-            aria-label="delete"
-          >
-            <Icon icon={faTimes} size="2x" color="white" />
-          </Button>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const Todos = ({ headerText }) => {
+  const dispatch = useDispatch();
+
+  const {
+    todosReducer: { todos },
+  } = useSelector((state) => state);
+
+  const onAdd = (todo) => {
+    dispatch(fetchPut({ ...todo, isComplete: false }));
+  };
+
+  const onUpdate = (id, data) => {
+    dispatch(fetchPatch({ id, data }));
+  };
+
+  const onDelete = (id) => {
+    dispatch(fetchDelete(id));
+  };
+
+  useEffect(() => {
+    dispatch(fetchGet());
+  }, [dispatch]);
+
+  return (
+    <div className="todo-list">
+      <Header
+        backgroundColor="#1f2a30"
+        logoText={<span style={{ marginLeft: '8px' }}>{headerText}</span>}
+        logoFontSize={36}
+        logoFontColor="white"
+        logoFont="'Barlow Condensed', Arial, Helvetica, sans-serif"
+        linkColor="white"
+        menuToggleColor="white"
+      />
+      <AddTodoForm onSubmit={(title) => onAdd({ title })} />
+      <ul>
+        {todos.map(({ id, title, isComplete }) => (
+          <li key={id}>
+            <Button
+              className="todo-list__status-btn"
+              size="sm"
+              checked={isComplete}
+              theme={isComplete ? 'lime' : 'red'}
+              onClick={() => onUpdate(id, { isComplete: !isComplete })}
+              aria-label={isComplete ? 'done' : 'pending'}
+              title="click to update status"
+            />
+            <FormField
+              aria-label="todo title"
+              className="todo-list__input"
+              defaultValue={title}
+              onBlur={(e) => onUpdate(id, { title: e.target.value })}
+            />
+            <Button
+              className="todo-list__delete-btn"
+              theme="hollow"
+              size="sm"
+              onClick={() => onDelete(id)}
+              aria-label="delete"
+            >
+              <Icon icon={faTimes} size="2x" color="white" />
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 Todos.propTypes = {
   headerText: PropTypes.string,
@@ -73,4 +98,4 @@ Todos.defaultProps = {
   onUpdate: null,
 };
 
-export default withEventHandlers(Todos);
+export default Todos;
