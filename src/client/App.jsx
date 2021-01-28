3ddfa022595a3fcfda303db082555b05d08f3431
Alan Eicker <alaneicker@gmail.com>
2021-01-28 10:52:25 -0600

    Adds Suspense and React.lazy for Todos

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { useSelector } from 'react-redux';
-import { Alert } from '@atomikui/core';
-import Todos from './components/Todos';
+import { Alert, Overlay, Spinner } from '@atomikui/core';
+
+const Todos = lazy(() => import('./components/Todos'));
 
 const App = () => {
   const {
@@ -9,18 +10,26 @@ const App = () => {
   } = useSelector((state) => state);
 
   return (
-    <div className="App">
-      <main>
-        {message && (
-          <Alert className="margin-top-16" theme={message.type}>
-            {message.text}
-          </Alert>
-        )}
-        {(!message || message.type !== 'error') && (
-          <Todos headerText="My Todos" />
-        )}
-      </main>
-    </div>
+    <Suspense
+      fallback={
+        <Overlay isActive>
+          <Spinner theme="white" themeVariant="light" size="lg" />
+        </Overlay>
+      }
+    >
+      <div className="App">
+        <main>
+          {message && (
+            <Alert className="margin-top-16" theme={message.type}>
+              {message.text}
+            </Alert>
+          )}
+          {(!message || message.type !== 'error') && (
+            <Todos headerText="My Todos" />
+          )}
+        </main>
+      </div>
+    </Suspense>
   );
 };
 
