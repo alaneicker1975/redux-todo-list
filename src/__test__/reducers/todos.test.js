c77159da34a7fccd4783d9f1910c409a1b2e8202
Alan Eicker <alaneicker@gmail.com>
2021-01-31 06:21:08 -0600

    Test coverage

--- src/__test__/reducers/todos.test.js
+++ src/__test__/reducers/todos.test.js
@@ -1,4 +1,5 @@
 import todosReducer, { initalState } from '../../client/reducers/todos';
+import * as types from '../../client/actions/types';
 
 describe('Todos reducer', () => {
   it('should return the intial state', () => {
@@ -9,7 +10,7 @@ describe('Todos reducer', () => {
     const payload = [{ id: 1, title: 'My Todo', isComplete: true }];
     expect(
       todosReducer(undefined, {
-        type: 'SET_TODOS',
+        type: types.SET_TODOS,
         payload,
       }),
     ).toEqual({ todos: payload });
@@ -19,11 +20,21 @@ describe('Todos reducer', () => {
     const payload = { id: 1, title: 'My Todo', isComplete: true };
     expect(
       todosReducer(undefined, {
-        type: 'ADD_TODO',
+        type: types.ADD_TODO,
         payload,
       }),
     ).toEqual({
       todos: [payload],
     });
+  });
+
+  it('Should delete a todo', () => {
+    initalState.todos = [{ id: 1, title: 'My Todo', isComplete: true }];
+    expect(
+      todosReducer(undefined, {
+        type: types.DELETE_TODO,
+        payload: 1,
+      }),
+    ).toEqual({ todos: [] });
   });
 });
