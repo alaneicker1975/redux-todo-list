b036e09b65b85508387b241c47c19106a49e7969
Alan Eicker <alaneicker@gmail.com>
2021-01-31 07:01:54 -0600

    Update todos.test.js

--- src/__test__/reducers/todos.test.js
+++ src/__test__/reducers/todos.test.js
@@ -2,6 +2,10 @@ import todosReducer, { initalState } from '../../client/reducers/todos';
 import * as types from '../../client/actions/types';
 
 describe('Todos reducer', () => {
+  beforeEach(() => {
+    initalState.todos = [{ id: 1, title: 'My Todo', isComplete: false }];
+  });
+
   it('should return the intial state', () => {
     expect(todosReducer(undefined, {})).toEqual(initalState);
   });
@@ -24,17 +28,25 @@ describe('Todos reducer', () => {
         payload,
       }),
     ).toEqual({
-      todos: [payload],
+      todos: [...initalState.todos, payload],
     });
   });
 
   it('Should delete a todo', () => {
-    initalState.todos = [{ id: 1, title: 'My Todo', isComplete: true }];
     expect(
       todosReducer(undefined, {
         type: types.DELETE_TODO,
         payload: 1,
       }),
     ).toEqual({ todos: [] });
+  });
+
+  it('Should update a todo', () => {
+    expect(
+      todosReducer(undefined, {
+        type: types.UPDATE_TODO,
+        payload: { id: 1, data: { isComplete: true } },
+      }),
+    ).toEqual({ todos: [{ id: 1, title: 'My Todo', isComplete: true }] });
   });
 });
